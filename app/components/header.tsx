b512c0763ae547065870cfcdd0a6a6f5494347d3
lord2202009 <azizbekabdurahimov2209@gmail.com>
2025-07-03 18:38:57 +0500

    market-rp

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,6 +3,8 @@
 import { Bell, Search, User } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import Link from "next/link"
+import { useState } from "react"
 
 interface HeaderProps {
   user: {
@@ -13,29 +15,21 @@ interface HeaderProps {
 }
 
 export default function Header({ user }: HeaderProps) {
+  const [load, setLoad] = useState(false)
   return (
     <header className="bg-white shadow-sm border-b px-6 py-4">
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between base">
         <div className="flex items-center space-x-4">
-          <div className="relative">
-            <Search className="absolute left-3 top-2.5 h-4 w-4 text-gray-400" />
-            <Input placeholder="Qidirish..." className="pl-10 w-80" />
-          </div>
+          <Link href="" className="text-4xl font-bold">Market Rp</Link>
         </div>
 
-        <div className="flex items-center space-x-4">
-          <Button variant="ghost" size="sm">
-            <Bell className="h-4 w-4" />
-          </Button>
-
-          <div className="flex items-center space-x-2">
-            <div className="bg-indigo-600 p-2 rounded-full">
-              <User className="h-4 w-4 text-white" />
-            </div>
-            <div className="text-sm">
-              <p className="font-medium">{user.name}</p>
-              <p className="text-gray-600">{user.email}</p>
-            </div>
+        <div className="flex items-center space-x-2">
+          <div className="bg-indigo-600 p-2 rounded-full">
+            <User className="h-4 w-4 text-white" />
+          </div>
+          <div className="text-sm">
+            <p className="font-medium">{user.name}</p>
+            <p className="text-gray-600">{user.email}</p>
           </div>
         </div>
       </div>
